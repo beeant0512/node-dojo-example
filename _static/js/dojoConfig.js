909b513d015ac0816e03d864398e4beb4439b710
Beeant <huangxb0512@gmail.com>
2016-12-21 23:29:19 +0800

    staff

--- _static/js/dojoConfig.js
+++ _static/js/dojoConfig.js
@@ -2,23 +2,27 @@
  * Created by beeant on 2016/11/21.
  */
 var dojoConfig = {
-  async: true,
-  parseOnLoad: 1,
-  // This code registers the correct location of the "demo"
-  // package so we can load Dojo from the CDN whilst still
-  // being able to load local modules
-  packages: [
-    {
-      name: "dojo-ext",
-      location: '/js/dojo-ext'
-    },
-    {
-      name: "dgrid",
-      location: '/assets/dgrid'
-    },
-    {
-      name: "dstore",
-      location: '/assets/dstore'
-    }
-  ]
+    async: true,
+    parseOnLoad: 1,
+    // This code registers the correct location of the "demo"
+    // package so we can load Dojo from the CDN whilst still
+    // being able to load local modules
+    packages: [
+        {
+            name: 'dojo',
+            location: '/nodelib/dojo'
+        },
+        {
+            name: "dojo-ext",
+            location: '/js/dojo-ext'
+        },
+        {
+            name: "dgrid",
+            location: '/nodelib/dgrid'
+        },
+        {
+            name: "dstore",
+            location: '/nodelib/dojo-dstore'
+        }
+    ]
 };
--- generate_menu_for_dojo_files.js
+++ generate_menu_for_dojo_files.js
@@ -1,139 +1,155 @@
 var fs = require('fs');
 var path = require('path');
-var rootPath = "H:/github/dojo-example/bower_components";
-var dojoPath = rootPath + "/dojo";
-var dojoxPath = rootPath + "/dojox";
-var dijitPath = rootPath + "/dijit";
-var dojoUtilPath = rootPath + "/dojo-util";
+var rootPath = "H:/github/node-dojo-example/node_modules";
+var menuRootPath = [
+    rootPath + "/dojo",
+    rootPath + "/dojox",
+    rootPath + "/dijit",
+    rootPath + "/dojo-util",
+    rootPath + "/dgrid"
+]
 var Menu = function (id, name, link) {
-  this.id = id;
-  this.name = name;
-  this.link = link;
-  this.class = "";
-  this.children = [];
+    this.id = id;
+    this.name = name;
+    this.link = link;
+    this.class = "";
+    this.children = [];
 };
 var appMenu = [
-  {
-    "id": "dojo",
-    "name": "Dojo",
-    "class": "active",
-    "children": [
-      {
-        "id": "dojo-html",
-        "name": "example",
+    {
+        "id": "dojo",
+        "name": "Dojo",
+        "class": "active",
+        "children": [
+            {
+                "id": "dojo-html",
+                "name": "example",
+                "class": "",
+                "children": []
+            }, {
+                "id": "dojo-md",
+                "name": "document",
+                "class": "",
+                "children": []
+            }]
+    },
+    {
+        "id": "digit",
+        "name": "Digit",
         "class": "",
-        "children": []
-      }, {
-        "id": "dojo-md",
-        "name": "document",
+        "children": [
+            {
+                "id": "digit-html",
+                "name": "example",
+                "class": "",
+                "children": []
+            }, {
+                "id": "digit-md",
+                "name": "document",
+                "class": "",
+                "children": []
+            }]
+    },
+    {
+        "id": "dojox",
+        "name": "Dojox",
         "class": "",
-        "children": []
-      }]
-  },
-  {
-    "id": "digit",
-    "name": "Digit",
-    "class": "",
-    "children": [
-      {
-        "id": "digit-html",
-        "name": "example",
+        "children": [
+            {
+                "id": "dojox-html",
+                "name": "example",
+                "class": "",
+                "children": []
+            }, {
+                "id": "dojox-md",
+                "name": "document",
+                "class": "",
+                "children": []
+            }]
+    },
+    {
+        "id": "util",
+        "name": "Util",
         "class": "",
-        "children": []
-      }, {
-        "id": "digit-md",
-        "name": "document",
+        "children": [
+            {
+                "id": "util-html",
+                "name": "example",
+                "class": "",
+                "children": []
+            }, {
+                "id": "util-md",
+                "name": "document",
+                "class": "",
+                "children": []
+            }]
+    },
+    {
+        "id": "dgrid",
+        "name": "dgrid",
         "class": "",
-        "children": []
-      }]
-  },
-  {
-    "id": "dojox",
-    "name": "Dojox",
-    "class": "",
-    "children": [
-      {
-        "id": "dojox-html",
-        "name": "example",
+        "children": [
+            {
+                "id": "dgrid-html",
+                "name": "example",
+                "class": "",
+                "children": []
+            }, {
+                "id": "dgrid-md",
+                "name": "document",
+                "class": "",
+                "children": []
+            }]
+    },
+    {
+        "id": "example",
+        "name": "Example",
+        "link": "",
         "class": "",
-        "children": []
-      }, {
-        "id": "dojox-md",
-        "name": "document",
-        "class": "",
-        "children": []
-      }]
-  },
-  {
-    "id": "util",
-    "name": "Util",
-    "class": "",
-    "children": [
-      {
-        "id": "util-html",
-        "name": "example",
-        "class": "",
-        "children": []
-      }, {
-        "id": "util-md",
-        "name": "document",
-        "class": "",
-        "children": []
-      }]
-  },
-  {
-    "id": "example",
-    "name": "Example",
-    "link": "",
-    "class": "",
-    "children": [{
-      "id": "example-flat",
-      "name": "flat",
-      "link": "/flat.html"
-    }, {
-      "id": "example-tree-remote",
-      "name": "remote tree",
-      "link": "/example/tree/remote.html"
-    }]
-  }
+        "children": [{
+            "id": "example-flat",
+            "name": "flat",
+            "link": "/flat.html"
+        }, {
+            "id": "example-tree-remote",
+            "name": "remote tree",
+            "link": "/example/tree/remote.html"
+        }]
+    }
 ];
 
-folder(dojoPath, appMenu[0].children[1].children, "doc-", '.md');
-folder(dojoPath, appMenu[0].children[0].children, "eg-", '.html');
+/*for(var i=0;i< appMenu.length - 1; i++){
+ folder(dojoPath, appMenu[i].children[1].children, "doc-", '.md');
+ folder(dojoPath, appMenu[i].children[0].children, "eg-", '.html');
+ }*/
 
-folder(dijitPath, appMenu[1].children[1].children, "doc-", '.md');
-folder(dijitPath, appMenu[1].children[0].children, "eg-", '.html');
-
-folder(dojoxPath, appMenu[2].children[1].children, "doc-", '.md');
-folder(dojoxPath, appMenu[2].children[0].children, "eg-", '.html');
-
-folder(dojoUtilPath, appMenu[3].children[1].children, "doc-", '.md');
-folder(dojoUtilPath, appMenu[3].children[0].children, "eg-", '.html');
+folder(menuRootPath[4], appMenu[4].children[1].children, "doc-", '.md');
+folder(menuRootPath[4], appMenu[4].children[0].children, "eg-", '.html');
 
 function folder(filepath, menus, prefix, linkext) {
-  var files = fs.readdirSync(filepath);
-  for (var i = 0; i < files.length; i++) {
-    var file = files[i];
-    var ext = path.extname(file);
-    if (ext != "" && ext != ".js") continue;
+    var files = fs.readdirSync(filepath);
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var ext = path.extname(file);
+        if (ext != "" && ext != ".js") continue;
 
-    var filename = file.replace(ext, "");
-    var filefullpath = filepath + "/" + file;
-    var stat = fs.statSync(filefullpath);
-    var menu = new Menu(prefix + filename, filename, "");
-    if (stat.isDirectory()) {
-      folder(filefullpath, menu.children, prefix, linkext);
-    } else if (stat.isFile()) {
-      var link = filefullpath.replace(rootPath, "");
-      link = link.replace(ext, linkext);
-      menu.link = link;
-      delete menu.children;
+        var filename = file.replace(ext, "");
+        var filefullpath = filepath + "/" + file;
+        var stat = fs.statSync(filefullpath);
+        var menu = new Menu(prefix + filename, filename, "");
+        if (stat.isDirectory()) {
+            folder(filefullpath, menu.children, prefix, linkext);
+        } else if (stat.isFile()) {
+            var link = filefullpath.replace(rootPath, "");
+            link = link.replace(ext, linkext);
+            menu.link = link;
+            delete menu.children;
+        }
+
+        menus.push(menu);
     }
 
-    menus.push(menu);
-  }
-
-  return menus;
+    return menus;
 }
 
-fs.writeFile(rootPath + "/menu.json", JSON.stringify(appMenu));
+fs.writeFile(rootPath + "/menu_gen.json", JSON.stringify(appMenu));
